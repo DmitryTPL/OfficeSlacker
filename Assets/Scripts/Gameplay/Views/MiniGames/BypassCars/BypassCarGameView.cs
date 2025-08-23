365b4a7ea40ba1dd035e4a86caee031fbcf893fc
Dmitrii Topal <dtopal@teamtiferet.com>
2025-08-23 17:37:35 +0200

    Bypass cars game road movement

---
+++ Assets/Scripts/Gameplay/Views/MiniGames/BypassCars/BypassCarGameView.cs
@@ -0,0 +1,13 @@
+﻿using Gameplay.Presenters;
+using MVP;
+using UnityEngine;
+
+namespace Gameplay.Views
+{
+    public class BypassCarGameView : View<BypassCarGamePresenter>
+    {
+        [SerializeField] private BypassCarGamePresenter.Data _data;
+
+        protected override BasePresenterViewSharedData SharedData => _data;
+    }
+}
